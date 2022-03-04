ac5f10b1ac1cdbf87111dce1733734a6048fe841
Victor Hugo Segundo Aguilar <viictorhugo.dev@gmail.com>
2022-03-03 19:57:03 -0600

    Configuraciones iniciales y conexión a BD

---
+++ db/resolvers.js
@@ -0,0 +1,8 @@
+//Resolvers
+const resolvers = {
+  Query: {
+    obtenerCurso: () => "Algo"
+  }
+}
+
+module.exports = resolvers;
---
+++ index.js
@@ -0,0 +1,22 @@
+const {ApolloServer} = require('apollo-server');
+const typeDefs = require('./db/schema');
+const resolvers = require('./db/resolvers');
+
+const conectarDB = require('./config/db');
+
+//Conectar a la base de datos
+conectarDB();
+
+//Servidor
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+
+});
+
+//Arrancando servidor
+server.listen().then(({url}) => {
+  console.log(`Servidor corriendo en la URL ${url}`)
+});
+
+
